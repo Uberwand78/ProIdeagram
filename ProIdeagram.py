6ca0143fa4ce3a8db322ff0920fed42ad05fff69
Irick Barfield <54927076+Uberwand78@users.noreply.github.com>
2020-06-28 15:53:10 -0400

    Update ProIdeagram.py

--- ProIdeagram.py
+++ ProIdeagram.py
@@ -4,7 +4,7 @@ import random
 def RunNotepad():
     os.system("notepad")
 
-# Generate 5 programming words from idea_gen_list
+# Generate 5 random programming words from idea_gen_list
 def Reload():
     print(" ")
     print("Here are some helpful programming keywords: ", random.choices(idea_gen_list, k=5))
@@ -21,14 +21,17 @@ idea_list_4 = ["Browser", "CD-R", "Link", "Template", "Website"]
 idea_list_5 = ["Notepad", "Login", "Beginner", "Intermediate", "Advanced"]
 idea_gen_list = idea_list_1 + idea_list_2 + idea_list_3 + idea_list_4 + idea_list_5
 
+# Opening statement
 print(" ")
-repeat = input("Start generating program ideas? Press (Enter) to start or (e) to exit " )
+repeat = input("Start generating program ideas? Press (Enter) to start or (e) to exit: " )
 
+# Body
 while repeat != "e":
     Reload()
     print(" ")
-    repeat = input("Try making another idea(s)? Press (Enter) to start or (e) to exit ")
+    repeat = input("Try making another idea(s)? Press (Enter) to start or (e) to exit: ")
+    repeat.lower()
     
-# Open notepad to transpose ideas     
+# Exit and open notepad to transpose ideas     
 if repeat == "e":
     RunNotepad()
