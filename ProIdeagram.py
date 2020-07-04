dd51d625bc995c004b3754a944032559256fd3a8
Irick Barfield <54927076+Uberwand78@users.noreply.github.com>
2020-07-04 03:46:41 -0400

    Update ProIdeagram.py

--- ProIdeagram.py
+++ ProIdeagram.py
@@ -6,14 +6,15 @@ def RunNotepad():
     os.system("notepad")
 
 # Generate 5 random, non-reoccuring programming words (using .sample) from idea_gen_list
-def Reload():
+def GenerateIdeas():
     print(" ")
-    print("Here are some helpful programming keywords: ", random.sample(idea_gen_list, k=5))
+    list_randomized = random.sample(idea_lists_combined, k=5)
+    print("Here are some helpful programming keywords: ", list_randomized)
     print(" ")
-    x = input("What is your program idea(s)? ")
-    x = x.title()
+    idea_input = input("What is your program idea(s)? ")
+    idea_input = idea_input.title()
     print(" ")
-    print("<> Program idea(s): ", x)
+    print("<> Program idea(s): ", idea_input)
 
 # List of lists
 idea_list_1 = ["Health", "Automation", "pdf", "Scraping", "Transfer"]
@@ -21,19 +22,19 @@ idea_list_2 = ["Tech", "GUI", "Money" ,"School", "Work"]
 idea_list_3 = ["Positive", "Dashboard", "Email", "Algorithm", "Bandwidth"]
 idea_list_4 = ["Browser", "CD-R", "Link", "Template", "Website"]
 idea_list_5 = ["Notepad", "Login", "Beginner", "Intermediate", "Advanced"]
-idea_gen_list = idea_list_1 + idea_list_2 + idea_list_3 + idea_list_4 + idea_list_5
+idea_lists_combined = idea_list_1 + idea_list_2 + idea_list_3 + idea_list_4 + idea_list_5
 
 # Opening statement
 print(" ")
-repeat = input("Start generating program ideas? Press (Enter) to start or (e) to exit: ")
-repeat = repeat.lower()
+start_exit = input("Start generating program ideas? Press (Enter) to start or (e) to exit: ")
+start_exit = start_exit.lower()
 
 # Body
-while repeat != "e":
-    Reload()
+while start_exit != "e":
+    GenerateIdeas()
     print(" ")
-    repeat = input("Try making another idea(s)? Press (Enter) to start or (e) to exit: ")
-    repeat.lower()
+    start_exit = input("Try making another idea(s)? Press (Enter) to start or (e) to exit: ")
+    start_exit.lower()
     
 # Exit and open Notepad
 if repeat == "e":
