ff919cb5eb866b042be8b310a48315ecee726e05
Irick Barfield <54927076+Uberwand78@users.noreply.github.com>
2020-08-31 16:54:48 -0400

    Update ProIdeagram.py

--- ProIdeagram.py
+++ ProIdeagram.py
@@ -1,40 +1,63 @@
-import random, os
+from os import system
+from random import sample
 
 # Open Notepad
 def RunNotepad():
-    os.system("notepad")
-
-# Generate 5 random, non-reoccuring programming words (.sample) from idea_lists_combined
+    system('notepad')
+    
+# Generate 5 random, non-reoccuring programming words (using .sample) from idea_lists_combined
 def GenerateIdeas():
-    print("")
-    list_randomized = random.sample(idea_lists_combined, k=5)
-    list_randomized.sort()
-    print("Here are some helpful programming keywords: ", list_randomized)
-    print("")
-    idea_input = input("What is your program idea(s)? ")
-    idea_input = idea_input.title()
-    print("")
-    print("Program idea(s): ", idea_input)
+    listRandomized = sample(ideaListsCombined, k=5)
+    listRandomized.sort()
+    print ('Here are some helpful programming keywords: ', '[%s]' % ', '.join(map(str, listRandomized)))
+    print('')
+    ideaInput = input('What is your program idea(s)? ')
+    ideaInput = ideaInput.title()
+    ideaList.append(ideaInput)
+    print('')
+    print ('Program idea(s): ', '[%s]' % ', '.join(map(str, ideaList)))    
+
+ideaList = []
 
 # List of lists
-idea_list_1 = ["Health", "Automation", "PDF", "Scraping", "Transfer"]
-idea_list_2 = ["Tech", "GUI", "Money" ,"School", "Work"]
-idea_list_3 = ["Positive", "Dashboard", "Email", "Algorithm", "Bandwidth"]
-idea_list_4 = ["Browser", "CD-R", "Link", "Template", "Website"]
-idea_list_5 = ["Notepad", "Login", "Beginner", "Intermediate", "Advanced"]
-idea_lists_combined = idea_list_1 + idea_list_2 + idea_list_3 + idea_list_4 + idea_list_5
+ideaList1 = ['Health', 'Automation', 'PDF', 'Scraping', 'Transfer']
+ideaList2 = ['Tech', 'GUI', 'Money' ,'School', 'Work']
+ideaList3 = ['Positive', 'Dashboard', 'Email', 'Algorithm', 'Bandwidth']
+ideaList4 = ['Browser', 'CD-R', 'Link', 'Template', 'Website']
+ideaList5 = ['Notepad', 'Login', 'Beginner', 'Intermediate', 'Advanced']
+ideaListsCombined = ideaList1 + ideaList2 + ideaList3 + ideaList4 + ideaList5
 
 # Opening statement
-print("Welcome to ProIdeagram!")
-print("")
-start_exit = input("Start generating program ideas? Press (Enter) to start or (e) to exit: ")
+print('Welcome to ProIdeagram!')
+print('')
+print('Start generating program ideas?')
+print('')
+startExit = input('Press (Enter) to start or (e) to exit: ')
+startExit = startExit.lower()
+if startExit == 'e':
+    quit()
+print('')
 
 # Body
-while start_exit != "e":
+while startExit != 'e':
     GenerateIdeas()
-    print("")
-    start_exit = input("Try making another idea(s)? Press (Enter) to start or (e) to exit and open Notepad: ")
-    
-# Exit and open Notepad
-if start_exit == "e":
+    print('')
+    print('Make another idea(s)?')
+    print('')
+    startExit = input('Press (Enter) to start or (e) to exit and open Notepad: ')
+    startExit = startExit.lower()
+    print('')
+
+# Open Notepad before exiting to transpose ideas 
+if startExit == 'e':
+    for x in ideaList:
+        print(x)
+    print('')
+    sortList = input('Sort list? (y) or (n): ')
+    print('')
+    if sortList == 'y':
+        ideaList.sort()
+        for x in ideaList:
+            print(x)   
+    print('')
     RunNotepad()
